c988a337cd19ec4a51a6ced355a500dcc6797546
Wagner Magno Ramos Sales <wmsales@stefanini.com>
2017-02-06 16:47:13 -0300

    primeiro

---
+++ src/main/java/si120162/lab03/controller/IndexController.java
@@ -0,0 +1,18 @@
+package si120162.lab03.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class IndexController {
+	@RequestMapping(value = "/teste", method = RequestMethod.GET)
+	public ModelAndView getIndex(){
+		ModelAndView model = new ModelAndView();
+		model.setViewName("teste");
+		return model;
+		
+	}
+	
+}	
